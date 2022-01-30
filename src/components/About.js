82ce63a7058955e39275f0f8014a1474e15c03cc
GregT <taylor.gregory2@gmail.com>
2022-01-30 16:58:03 -0700

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,14 @@ import React from "react";
 import Links from "./Links";
 
 function About(props) {
+  console.log(props)
   if(props.bio){
     return (
       <div id="about">
         <h2>About Me</h2>
         <p>{props.bio}</p>
         <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-        <Links />
+        <Links github = {props.links.github} linkedin = {props.links.linkedin} />
       </div>
     );
     
@@ -17,7 +18,7 @@ function About(props) {
     <div id="about">
       <h2>About Me</h2>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links />
+      <Links github = {props.links.github} linkedin = {props.links.linkedin} />
     </div>
   );
 }
